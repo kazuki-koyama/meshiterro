a2e621b06ae3586d448453e8569181fbb2193cdc
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-03-17 00:17:21 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b2b31c00bc9e5ddf592a041301aa4a65496507b2c1e8adb930f377c011981e8aebd68b347fec0ea4106cc2a03b8575b547552b8f81a8d7da03859214646afb04'

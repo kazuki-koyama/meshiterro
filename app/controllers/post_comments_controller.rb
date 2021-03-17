b81a4db51e86ffaa04bd4d3a80920022f653b432
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-03-17 22:35:26 +0000

    [Fix] 4-4 コメント投稿機能

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,7 +3,7 @@ class PostCommentsController < ApplicationController
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = PostComment.new(post_comment_params)
-    comment_user_id = current_user.id
+    comment.user_id = current_user.id
     comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)

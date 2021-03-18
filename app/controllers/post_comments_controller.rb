f7f18f4bf17de20dad36ce0fddd459df1fe4b2ee
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-03-18 04:31:32 +0000

    [Finish] 4-4

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,7 +5,7 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     # comment = PostComment.new(post_comment_params)
     # comment.user_id = current_user.id
-    # comment.post_image_id = post_image.id
+    comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
   end

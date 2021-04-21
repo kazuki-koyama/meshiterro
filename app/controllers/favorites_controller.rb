8b1314f59603d98b33c24383c988e6629c9327f2
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-04-21 13:43:32 +0000

    [update] favorites#create

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,8 @@ class FavoritesController < ApplicationController
 
   def create
     post_image = PostImage.find(params[:post_image_id])
-    favorite = current_user.favorites.new(post_image_id: post_image.id)  # ストロングパラメータで受け取る値が無い場合、newの()には何を指定するのか？
-    # favorite = Favorite.new(???)
+    favorite = current_user.favorites.new(post_image_id: post_image.id)
+    # favorite = Favorite.new
     # favorite.user_id = current_user.id
     # favorite.post_image_id = post_image.id
     favorite.save
@@ -13,7 +13,6 @@ class FavoritesController < ApplicationController
   def destroy
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
-    # favorite.post_image_id = post_image.id
     favorite.destroy
     redirect_to post_image_path(post_image)
   end

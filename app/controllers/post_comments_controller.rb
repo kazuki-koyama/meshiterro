e8c011e9a5042bf22966aaa0504563d2e2731a90
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-04-21 08:53:46 +0000

    [Update] post_commentsコントローラ#create

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,14 +5,14 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     # comment = PostComment.new(post_comment_params)
     # comment.user_id = current_user.id
+
     comment.post_image_id = post_image.id
-    comment.save
+    comment.save　# ここで始めてコメントにidが振られる
     redirect_to post_image_path(post_image)
   end
 
   def destroy
     PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
-    # idを2つ指定しなければならない理由は、ルーティングをネストした結果、urlに両方のid指定が必要だから？
     redirect_to post_image_path(params[:post_image_id])
   end
 
